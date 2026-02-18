7ac1eb2c3d5f31589d03a237891bdecd09bd7da3
PRANTO KUMER PAUL <prantopaul55555@gmail.com>
2026-02-18 17:15:54 +0600

    User.model.js implemented

--- model/User.model.js
+++ model/User.model.js
@@ -1,6 +1,40 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      defalt: 'user',
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+    },
+    resetPasswordToken: {
+      type: String,
+    },
+    resetPasswordExpire: {
+      type: Date,
+    },
+  },
+  { timestams: true }
+);
 
 const User = mongoose.model('User', userSchema);
 
