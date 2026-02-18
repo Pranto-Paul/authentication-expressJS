b6ec67de13047592e410a8986025254404f6cb27
PRANTO KUMER PAUL <prantopaul55555@gmail.com>
2026-02-18 17:17:29 +0600

    user.controller.js boilerplate code done

---
+++ controller/user.controller.js
@@ -0,0 +1,10 @@
+export const register = async (req, res) => {
+  try {
+    res.send('register');
+  } catch (error) {}
+};
+export const login = async (req, res) => {
+  try {
+    res.send('login');
+  } catch (error) {}
+};
